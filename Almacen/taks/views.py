7799cc1b74e3520befafc38258e10a1ae98632de
Tulachin5cm <sebaherreran@gmail.com>
2025-08-24 23:02:21 -0400

    inicial

---
+++ Almacen/taks/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+
+def helloworld (request):
+    return render(request ,'singup.html',{
+        'form' : UserCreationForm
+        })
+ 
