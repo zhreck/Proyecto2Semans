92ce1af286b38e230623178ea641818022dd78e9
Tulachin5cm <sebaherreran@gmail.com>
2025-09-02 02:39:45 -0400

    arreglos

--- Almacen/storage/views.py
+++ Almacen/storage/views.py
@@ -6,25 +6,25 @@ from .models import Archivo
 from .serializers import ArchivoSerializer
 
 def inicio(request):
-    return render(request, './html/inicio.html')
+    return render(request, 'html/inicio.html')
 
 def acceso(request):
-    return render(request, './html/acceso.html')
+    return render(request, 'html/acceso.html')
 
 def inicio2(request):
-    return render(request, './html/Inicio2.html')
+    return render(request, 'html/Inicio2.html')
 
 def inicio3(request):
-    return render(request, './html/Inicio3.html')
+    return render(request, 'html/Inicio3.html')
 
 def productos(request):
-    return render(request, './html/productos.html')
+    return render(request, 'html/productos.html')
 
 def ayuda(request):
-    return render(request, './html/ayuda.html')
+    return render(request, 'html/ayuda.html')
 
 def registro(request):
-    return render(request,'./html/registro.html')
+    return render(request, 'html/registro.html')
 
 
 
